7931b58d3251beaaa537dd1f76a208de74fb8f5d
Lev1ty <adamxhy123@gmail.com>
2025-11-01 19:51:49 -0400

    Comment out IAM binding on deleted resource

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,10 +1,10 @@
 data "google_compute_default_service_account" "default" {}
 
-resource "google_artifact_registry_repository_iam_member" "marrow_artifact_registry_reader" {
-  repository = var.repository
-  role       = "roles/artifactregistry.reader"
-  member     = "serviceAccount:${data.google_compute_default_service_account.default.email}"
-}
+# resource "google_artifact_registry_repository_iam_member" "marrow_artifact_registry_reader" {
+#   repository = var.repository
+#   role       = "roles/artifactregistry.reader"
+#   member     = "serviceAccount:${data.google_compute_default_service_account.default.email}"
+# }
 
 resource "google_storage_bucket_iam_member" "risingwave_state_storage_user" {
   bucket = google_storage_bucket.risingwave_state.name
