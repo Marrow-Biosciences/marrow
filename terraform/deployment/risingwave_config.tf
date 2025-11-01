37365f9d7943908d3b7bbb68d0adec02abf41caa
Lev1ty <adamxhy123@gmail.com>
2025-11-01 19:41:28 -0400

    Temporarily untrack deployment
    Commenting out deployment module in main.tf does not deactivate it

--- terraform/deployment/risingwave_config.tf
+++
@@ -1,8 +0,0 @@
-resource "kubernetes_config_map_v1" "risingwave" {
-  metadata {
-    name = "risingwave-config"
-  }
-  data = {
-    "risingwave.toml" = file("${path.module}/risingwave.toml")
-  }
-}
